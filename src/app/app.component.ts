801900e36f9b0cc738db0c5be374bf4d00f81d96
SwathyInfomap <Swathy@infomapglobal.com>
2024-03-28 10:38:44 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from "./components/home/home.component";
+import { HeaderComponent } from './components/header/header.component';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [MatButtonModule, HomeComponent,HeaderComponent]
 })
 export class AppComponent {
-  title = 'product-management';
+  title="my product management";
+  a=5;
+  b=6;
 }
