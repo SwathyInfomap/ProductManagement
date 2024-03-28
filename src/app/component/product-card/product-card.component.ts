c526544a4c04af650d24cb28707dac970779453c
SwathyInfomap <Swathy@infomapglobal.com>
2024-03-28 13:36:42 +0530

    Material Card in the Product-card component created

---
+++ src/app/component/product-card/product-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css'
+})
+export class ProductCardComponent {
+
+}
