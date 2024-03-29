d8090de684289a80f4e29dee2865b1e13d26a787
SwathyInfomap <Swathy@infomapglobal.com>
2024-03-29 19:54:09 +0530

    added header component, product detail card and routing on click of view button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
-  template: ``,
-  styles: ``
+  imports: [MatButtonModule,MatIconModule,MatToolbarModule,RouterLink],
+  template: `  <mat-toolbar color="primary">
+  <button mat-icon-button routerLink="/">
+    <mat-icon>home</mat-icon>
+  </button>
+  <span>My Product Management</span>
+  </mat-toolbar>`,
+
 })
 export class HeaderComponent {
 
--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { Product } from '../../types/product';
+import { ProductService } from '../../product.service';
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule, MatButtonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
-
+  product!:Product;
+  productService=inject(ProductService);
+  ngOnInit(){
+    let productId=1;
+    this.productService.getProductById(productId).subscribe(result=>{
+      this.product=result;
+    })
+  }
 }
