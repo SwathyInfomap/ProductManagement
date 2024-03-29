6f9504ee28d40b0cc35b0e83b8a6aa4c57002562
SwathyInfomap <Swathy@infomapglobal.com>
2024-03-29 10:09:45 +0530

    Sending dynamic data from ProductCard Component to Home Component to display data in product cards

--- src/app/component/product-card/product-card.component.ts
+++ src/app/component/product-card/product-card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-
+  @Input() product:any;
 }
